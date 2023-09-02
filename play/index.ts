cba903274db22de08b5911f393cf0e517d663078
hunghg255 <giahunghust@gmail.com>
2023-09-02 08:03:52 +0700

    chore: update load js

--- play/index.ts
+++ play/index.ts
@@ -3,7 +3,7 @@ import { readConfig } from '../src/index';
 const start = () => {
   const data = readConfig('test', {
     default: {
-      testItem2: 'some value '
+      defaultV: 'default value '
     },
   });
 
--- src/loader/js.ts
+++ src/loader/js.ts
@@ -1,23 +1,11 @@
 import jitiFactory from 'jiti';
 import type { JITIOptions } from 'jiti/dist/types';
-import { transform, Options } from 'sucrase';
+import { Options } from 'sucrase';
 
-let jiti: ReturnType<typeof jitiFactory> | null = null;
-function lazyJiti(option: JITIOptions = {}, transformOpt = {} as Options) {
-  return (
-    jiti ??
-    (jiti = jitiFactory(__filename, {
-      interopDefault: true,
-      ...option,
-      transform: (opts) => {
-        return transform(opts.source, {
-          //@ts-ignore
-          transforms: ['typescript', 'imports'],
-          ...transformOpt,
-        });
-      },
-    }))
-  );
+function lazyJiti(rootDir:string = process.cwd(), option: JITIOptions = {}) {
+  const split = rootDir.split('/');
+  const _require = jitiFactory(rootDir, { interopDefault: true, esmResolve: true, ...option });
+  return _require(`./${split[split.length - 1]}`);
 }
 
 export interface LoadConfOption {
@@ -26,23 +14,23 @@ export interface LoadConfOption {
   transformOption?: Options;
 }
 
-export function loadConf<T>(path: string, option: LoadConfOption = {}): T {
-  const { jiti = true, jitiOptions, transformOption } = option;
+function loadConf<T>(path: string, option: LoadConfOption = {}): T {
+  const { jiti = true, jitiOptions } = option;
   let config = (function () {
     try {
       if (jiti) {
-        return path ? lazyJiti(jitiOptions, transformOption)(path) : {};
+        return path ? lazyJiti(path, jitiOptions): {};
       } else {
         return path ? require(path) : {};
       }
     } catch {
-      return lazyJiti(jitiOptions, transformOption)(path);
+      return lazyJiti(path, jitiOptions);
     }
   })();
   return config.default ?? config;
 }
 
 //@ts-ignore
-export function jsLoader<T>(filepath: string, content: string, option: LoadConfOption = {}): T {
+export const jsLoader =  <T>(filepath: string, content: string, option: LoadConfOption = {}): T => {
   return loadConf<T>(filepath, option);
 }
